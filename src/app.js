5e59f990394a9c2dfaf559efa237a2beaae3f57f
jenilmangukiya <jenilmangukiya2002@gmail.com>
2024-01-13 19:30:12 +0530

    [main] add comment comtroller

--- src/app.js
+++ src/app.js
@@ -17,17 +17,19 @@ app.use(express.static("public")); // Static Middleware
 app.use(cookieParser()); // Cookies Middleware
 
 // Routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import playlistRoute from "./routes/playlist.routes.js";
-import healthcheckRouter from "./routes/healthcheck.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 // Routes Declaration
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
-app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/playlist", playlistRoute);
+app.use("/api/v1/comments", commentRouter);
 
 export { app };
---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,116 @@
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ObjectId } from "mongodb";
+
+// Need to restrict user by USERID
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const {
+    page = 1,
+    limit = 10,
+    sortBy = "createdAt",
+    sortType = "desc",
+  } = req.query;
+
+  if (!mongoose.Types.ObjectId.isValid(videoId))
+    throw new ApiError(404, "Video not found");
+
+  const options = {
+    page: +page,
+    limit: +limit,
+  };
+
+  const sortOrder = sortType.toLowerCase() === "desc" ? -1 : 1;
+  const pipeline = Comment.aggregate([
+    {
+      $match: { video: new ObjectId(videoId) },
+    },
+    {
+      $sort: { [sortBy]: sortOrder },
+    },
+    {
+      $skip: (+page - 1) * +limit,
+    },
+    {
+      $limit: +limit,
+    },
+  ]);
+
+  const paginatedComments = await Comment.aggregatePaginate(pipeline, options);
+  if (!paginatedComments) {
+    throw new ApiError(400, "Failed to load comments!, Please try again ");
+  }
+
+  res.status(200).json(new ApiResponse(200, paginatedComments));
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  // check if the Video Id is in correct format
+  if (!mongoose.Types.ObjectId.isValid(videoId))
+    throw new ApiError(404, "Video not found");
+
+  if (!content) {
+    throw new ApiError(400, "Content is Required");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  res.status(200).json(new ApiResponse(201, comment, "Commented successfully"));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  // check if the Comment Id is in correct format
+  if (!mongoose.Types.ObjectId.isValid(commentId))
+    throw new ApiError(404, "Comment not found");
+
+  if (!content) {
+    throw new ApiError(200, "Content is required");
+  }
+
+  const comment = await Comment.findByIdAndUpdate(
+    commentId,
+    { $set: { content: content } },
+    { new: true }
+  );
+
+  if (!comment) {
+    throw new ApiError(200, "Failed to update Comment please Try Again!");
+  }
+  res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment updated Successfully"));
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+
+  // check if the Comment Id is in correct format
+  if (!mongoose.Types.ObjectId.isValid(commentId))
+    throw new ApiError(404, "Comment not found");
+
+  const comment = await Comment.findByIdAndDelete(commentId);
+
+  if (!comment) {
+    throw new ApiError("Failed to delete Comment!, Please try again");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, null, "Comment deleted successfully"));
+});
+
+export { getVideoComments, addComment, updateComment, deleteComment };
